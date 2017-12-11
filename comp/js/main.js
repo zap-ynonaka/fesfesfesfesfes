8ef4c86ebcc441710b369724fee11ce8dfbacc73
zap-k-namikawa <k_namikawa@zappallas.com>
2017-12-11 15:59:13 +0900

    attraction

--- comp/js/main.js
+++ comp/js/main.js
@@ -10632,3 +10632,34 @@ function topImageHidden(){
   $('.videoArea__images li').removeClass('active');
   $('.videoArea__images li:first-child').addClass('active');
 }
+
+$(function(){
+  $('.modal-open').click(function(){
+      $('body').append('<div class="modal-overlay"></div>');
+      $('.modal-overlay').fadeIn('slow');
+
+      var modal = '#' + $(this).attr('data-target');
+      modalResize();
+      $(modal).fadeIn('slow');
+
+      $('.modal-overlay, .modal-close').off().click(function(){
+          $(modal).fadeOut('slow');
+          $('.modal-overlay').fadeOut('slow',function(){
+              $('.modal-overlay').remove();
+          });
+      });
+
+      $(window).on('resize', function(){
+          modalResize();
+      });
+
+      function modalResize(){
+          var w = $(window).width();
+          var h = $(window).height();
+          var x = (w - $(modal).outerWidth(true)) / 2;
+          var y = (h - $(modal).outerHeight(true)) / 9;
+          $(modal).css({'left': x + 'px','top': y + 'px'});
+      }
+
+  });
+});
